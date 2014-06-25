a0e4842ad6494a824c23298622da190908accae4
bemoove <bemoove@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 09:44:29 +0000

    アカウントが設定されていない場合の挙動を修正
    git-svn-id: https://plugins.svn.wordpress.org/wp-bemoove/trunk@938385 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bemoove-config.php
+++ bemoove-config.php
@@ -2,10 +2,14 @@
 
 /** BeHLsサーバーのホスト名 */
 define('BEHLS_HOST_NAME', 'wordpress.behls-lite.jp');
+define('BEHLS_PROTOCOL', 'https');
+
+/** アカウント操作を行う代理サーバーのホスト名 */
+define('BEHLS_ADMIN_PROXY_HOST_NAME', 'todo');
 
 /**
  * admin_apikey
  * アカウント操作権限をこのプラグインに委譲する場合は設定が必要
  */
-define('ADMIN_APIKEY', '');
+define('ADMIN_APIPREKEY', '');
 ?>
